20bc6933bd412b43d63ded5af1408934797492dc
Channveer Hiremath <163711050+channveerswamiHiremath@users.noreply.github.com>
2025-10-08 19:06:35 +0530

    Add files via upload

---
+++ Search_insert_posistion(35).py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+            if left < len(nums) and nums[left] >= target:
+                return left
+            if right >= 0 and nums[right] < target and left >= len(nums):
+                return right + 1
+        return left
